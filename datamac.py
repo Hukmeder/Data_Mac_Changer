00c5c2b7cedf26049539ea13f7e88b06314fad8c
Alper <serviamsezgin@gmail.com>
2025-08-07 13:09:30 +0300

    Update datamac.py

--- datamac.py
+++ datamac.py
@@ -14,16 +14,17 @@ def generate_unique_macs(count=2):
         macs.add(mac)
     return list(macs)
 
-# MAC adreslerini üret
+# Produce MAC Adress
 macs = generate_unique_macs(2)
 
-# Hata kontrolü
+# Check
 print("Toplam MAC sayısı:", len(macs))
 print("MAC listesi:", macs)
 
-# Ekrana yazdır
+# Print
 if len(macs) >= 2:
     print("MAC 1 (örneğin LAN):  ", macs[0])
     print("MAC 2 (örneğin Wi-Fi):", macs[1])
 else:
     print("Yeterli sayıda MAC üretilemedi.")
+
